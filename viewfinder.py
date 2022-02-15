3e31f50795ca365e4ac27ae9123bcd020dd35f13
Yagiz Gunes Teker <gunes.teker@gmail.com>
2022-02-16 00:19:04 +0100

    checked elements removed

--- viewfinder.py
+++ viewfinder.py
@@ -1,17 +1,24 @@
 import json
 import itertools
+from os import remove
 import sys
 
 count = 0
 
-if len(sys.argv) == 3:
-    count = int(sys.argv[2])
-file_name = sys.argv[1]
+# if len(sys.argv) == 3:
+#     count = int(sys.argv[2])
+# file_name = sys.argv[1]
 
-print(len(sys.argv))
+# print(len(sys.argv))
 
-with open(''+file_name) as f:
+# with open(''+file_name) as f:
+#     data = json.load(f)
+
+with open("mesh_x_sin_cos_10000.json") as f:
     data = json.load(f)
+
+candidates = data['elements']
+eliminated = []
 
 def inTriangle(mNodeId, mTriangle):
     return mNodeId in [nodeId for nodeId in mTriangle['nodes']]
@@ -29,19 +36,35 @@ def findValues(neighbours):
         res.append({'id': triangle['id'], 'value': next(valueEl['value'] for valueEl in data['values'] if valueEl['element_id'] == triangle['id'])})
     return res
 
+def removeElements(ids):
+    temp = [rectangle for rectangle in candidates if rectangle['id'] in ids]
+    for el in temp:
+        candidates.remove(el)
+
+
 def isVertex(triangle, neighbours):
+    triangleValue = 0
+    for temp in neighbours:
+        if temp['id'] == triangle['id']:
+            triangleValue = temp['value']
     neighbours.sort(key=lambda x: x['value'])
     if neighbours[-1]['id'] == triangle['id']:
+        removeElements([item['id'] for item in neighbours])
         return {'isVertex': True, 'value': neighbours[-1]['value']}
-    return {'isVertex': False, 'value': neighbours[-1]['value']}
+    else:
+        removeElements([item['id'] for item in neighbours[:-1] if item['value'] < triangleValue])
+        return {'isVertex': False, 'value': neighbours[-1]['value']}
+
 
 def findVertices():
     res = []
-    for triangle in data['elements']:
+    for triangle in candidates:
         if count > len(res) or count == 0:
             temp = isVertex(triangle, findValues(findNeighbours(triangle)))
             if temp['isVertex']:
                 res.append({'id': triangle['id'], 'value': temp['value']})
     return res
+
+
 
 print(findVertices())
